b83d8c6c7cf50b16326a578b3aba32fc532a3227
salma azza <salma.azza90@gmail.com>
2025-12-26 16:39:41 +0100

    salam

--- api/app.py
+++ api/app.py
@@ -3,8 +3,11 @@ import sqlite3
 import subprocess
 import hashlib
 import os
+
 app = Flask(__name__)
-SECRET_KEY = "dev-secret-key-12345" # Hardcoded secret
+SECRET_KEY = "dev-secret-key-12345"  # Hardcoded secret
+
+
 @app.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username")
@@ -25,23 +28,25 @@ def login():
 @app.route("/ping", methods=["POST"])
 def ping():
     host = request.json.get("host", "")
-    cmd = f"ping -c 1 {host}"
-    output = subprocess.check_output(cmd, shell=True)
+    output = subprocess.check_output(
+        ["ping", "-c", "1", host], stderr=subprocess.STDOUT, timeout=5
+    )
 
     return {"output": output.decode()}
+
 
 @app.route("/compute", methods=["POST"])
 def compute():
     expression = request.json.get("expression", "1+1")
-    result = eval(expression) # CRITIQUE
+    result = eval(expression)  # CRITIQUE
     return {"result": result}
 
 
 @app.route("/hash", methods=["POST"])
 def hash_password():
     pwd = request.json.get("password", "admin")
-    hashed = hashlib.md5(pwd.encode()).hexdigest()
-    return {"md5": hashed}
+    hashed = hashlib.sha256(pwd.encode()).hexdigest()
+    return {"sha256": hashed}
 
 
 @app.route("/readfile", methods=["POST"])
@@ -51,20 +56,18 @@ def readfile():
         content = f.read()
 
     return {"content": content}
+
+
 @app.route("/debug", methods=["GET"])
 def debug():
     # Renvoie des détails sensibles -&gt; mauvaise pratique
-    return {
-    "debug": True,
-    "secret_key": SECRET_KEY,
-    "environment": dict(os.environ)
-    }
-    
-    
+    return {"debug": True, "secret_key": SECRET_KEY, "environment": dict(os.environ)}
+
+
 @app.route("/hello", methods=["GET"])
 def hello():
     return {"message": "Welcome to the DevSecOps vulnerable API"}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000) 
